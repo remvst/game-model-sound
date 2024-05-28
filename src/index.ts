e2a980d89e55784f516fd521a157c7fb1e863b76
remvst <remvst@gmail.com>
2024-05-28 17:02:04 -0400

    Gerrrrrr

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "./ambient-sound-effect-controller";
 export * from "./continuous-sound-effect-controller";
 export * from "./simple-sound-effect-controller";
 export * from "./sound-effect-controller";
